85776a42479885a46b95e18d869ce91e00fec63c
Bogdan Bobe <33196341+arealshadow@users.noreply.github.com>
2025-05-26 19:44:49 +0300

    first commit

---
+++ app/src/main/java/com/shadoapps/fitmi/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.shadoapps.fitmi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebViewFeature
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            // A surface container using the 'background' color from the theme
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                WebViewScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun WebViewScreen() {
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                webViewClient = WebViewClient() // Handle navigation within WebView
+                loadUrl("file:///android_asset/index.html")
+            }
+        },
+        modifier = Modifier.fillMaxSize()
+    )
+}
